683e31e3e5cee13e92b483af92825dab52bf19d3
WaiYan98 <wyh.waiyan98@gmail.com>
2025-12-24 22:28:33 +0630

    Backup current feature progress

--- composeApp/src/commonMain/kotlin/com/waiyan/myittar_oo_emr/screen/component/patient_history_screen/PatientHistoryScreen.kt
+++ composeApp/src/commonMain/kotlin/com/waiyan/myittar_oo_emr/screen/component/patient_history_screen/PatientHistoryScreen.kt
@@ -33,7 +33,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
